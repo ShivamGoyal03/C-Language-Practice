5c26e03f7afa8d30390939df052ca554620e03a6
ShivamDev001 <sggarg1976@gmail.com>
2021-10-03 18:28:42 +0530

    Random Number Guesser

---
+++ random_guesser.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    int number;
+    srand(time(0));
+    for (int i = 0; i < 1; i++)
+    {
+        printf("%d",rand());
+    }
+    
+    return 0;
+}
