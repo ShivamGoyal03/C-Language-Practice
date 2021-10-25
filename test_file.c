e86548b8b8aec21258a219636ae441600835ddc6
ShivamDev001 <sggarg1976@gmail.com>
2021-10-25 21:35:53 +0530

    prime number

---
+++ test_file.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+   int i, num, j = 0;
+   printf("Enter a number: \n");
+   scanf("%d", &num);
+   for(i=1; i<=num; i++)
+   {
+      if(num%i==0)
+      {
+         j++;
+      }
+   }
+   if(j==2)
+   {
+      printf("Prime number");
+   }
+   else
+   {
+      printf("Not a prime number");
+   }
+}
