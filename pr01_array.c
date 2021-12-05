e35688dc29178d61174fd8430f9875e6bd9e756f
Shivam-090 <shivam21csu090@ncuindia.edu>
2021-12-05 15:59:20 +0530

    Arrays

---
+++ pr01_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int i = 5;
+    int* ptr = &i;
+    printf("The value of i is %d\n",i);
+    printf("The value of ptr is %u\n",ptr);
+    printf("The address of 1 is %u\n",&i);
+    ptr++; // Addition of Arrays
+    printf("\n");
+    printf("The value of ptr is %u\n",ptr);
+    printf("The address of 1 is %u\n",&i);
+    ptr--; // Subtraction of Arrays
+    printf("\n");
+    printf("The value of ptr is %u\n",ptr);
+    printf("The address of 1 is %u\n",&i);
+
+    return 0;
+}
